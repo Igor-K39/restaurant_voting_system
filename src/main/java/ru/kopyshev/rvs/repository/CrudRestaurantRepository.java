b19ddafaa0cec6a47d6b163ac1d398bb4f19d770
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-10-16 04:40:23 +0300

    implement repository: CrudRestaurantRepository.java

---
+++ src/main/java/ru/kopyshev/rvs/repository/CrudRestaurantRepository.java
@@ -0,0 +1,24 @@
+package ru.kopyshev.rvs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kopyshev.rvs.model.Restaurant;
+
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public interface CrudRestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Restaurant r WHERE r.id = :id")
+    int delete(@Param("id") int id);
+
+    @Query("SELECT r FROM Restaurant r ORDER BY r.name, r.address")
+    List<Restaurant> getAll();
+}
