c51dd1a8a6bcd1833e1c198964a38a6e64615b47
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-10-18 17:41:37 +0300

    implement to: BaseTo.java

---
+++ src/main/java/ru/kopyshev/rvs/to/BaseTo.java
@@ -0,0 +1,45 @@
+package ru.kopyshev.rvs.to;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BaseTo {
+
+    protected Integer id;
+
+    public BaseTo(Integer id) {
+        this.id = id;
+    }
+
+    @JsonIgnore
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseTo that = (BaseTo) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "id=" + id +
+                '}';
+    }
+}
