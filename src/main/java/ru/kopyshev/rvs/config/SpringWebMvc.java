83938a4d832cebbc34f25725a7a8c3033621a400
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-10-22 21:09:52 +0300

    implement config: SpringWebMvc.java

---
+++ src/main/java/ru/kopyshev/rvs/config/SpringWebMvc.java
@@ -0,0 +1,45 @@
+package ru.kopyshev.rvs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import ru.kopyshev.rvs.web.converter.StringToLocalDateConverter;
+import ru.kopyshev.rvs.web.converter.StringToLocalTimeConverter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = ApplicationProperties.WEB_PACKAGE)
+public class SpringWebMvc implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToLocalTimeConverter());
+        registry.addConverter(new StringToLocalDateConverter());
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+}
