71d1abb4567b7b51db23094aa07cef9fe9cfb5e6
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-10-22 21:11:35 +0300

    implement test: JsonUtilTest.java

---
+++ src/test/java/ru/kopyshev/rvs/web/json/JsonUtilTest.java
@@ -0,0 +1,31 @@
+package ru.kopyshev.rvs.web.json;
+
+import org.junit.jupiter.api.Test;
+import ru.kopyshev.rvs.model.User;
+import ru.kopyshev.rvs.util.JsonUtil;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static ru.kopyshev.rvs.UserTestData.*;
+import static ru.kopyshev.rvs.UserTestData.USER_MATCHER;
+
+public class JsonUtilTest {
+
+    @Test
+    void readWriteValue() {
+        String json = JsonUtil.writeValue(USER);
+        System.out.println(json);
+        User user = JsonUtil.readValue(json, User.class);
+        USER_MATCHER.assertMatch(user, USER);
+    }
+
+    @Test
+    void readWriteValues() {
+        List<User> expected = Arrays.asList(USER, ADMIN);
+        String json = JsonUtil.writeValue(expected);
+        System.out.println(json);
+        List<User> actual = JsonUtil.readValues(json, User.class);
+        USER_MATCHER.assertMatch(actual, expected);
+    }
+}
