b1c9f60294ad70f9ded09ffc88b29614a105cac9
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-10-22 19:59:29 +0300

    implement web: ProfileRestController.java

---
+++ src/main/java/ru/kopyshev/rvs/web/user/ProfileRestController.java
@@ -0,0 +1,48 @@
+package ru.kopyshev.rvs.web.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.kopyshev.rvs.to.UserTo;
+import ru.kopyshev.rvs.web.SecurityUtil;
+
+import java.net.URI;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = ProfileRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class ProfileRestController extends AbstractUserController{
+    static final String REST_URL = "/rest/profile";
+
+    @PostMapping("/register")
+    public ResponseEntity<UserTo> createWithLocation(@RequestBody UserTo userTo) {
+        UserTo created = super.create(userTo);
+        URI uriOfNewResource = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @GetMapping("")
+    public UserTo get() {
+        int userId = SecurityUtil.authUserId();
+        return super.get(userId);
+    }
+
+    @PatchMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void patch(@RequestBody Map<String, Object> updates) {
+        int userId = SecurityUtil.authUserId();
+        super.update(updates, userId, "name", "email", "password");
+    }
+
+    @DeleteMapping("")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete() {
+        int userId = SecurityUtil.authUserId();
+        super.delete(userId);
+    }
+}
