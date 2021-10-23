cf48b4c41e5edb97490af86906feee1e342b18b7
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-10-23 15:34:54 +0300

    implement web: UserRestaurantController.java

---
+++ src/main/java/ru/kopyshev/rvs/web/restaurant/UserRestaurantController.java
@@ -0,0 +1,28 @@
+package ru.kopyshev.rvs.web.restaurant;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kopyshev.rvs.to.RestaurantTo;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = UserRestaurantController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserRestaurantController extends AbstractRestaurantController {
+    public static final String REST_URL = "/rest/restaurants";
+
+    @Override
+    @GetMapping("/{id}")
+    public RestaurantTo get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<RestaurantTo> getAll() {
+        return super.getAll();
+    }
+}
