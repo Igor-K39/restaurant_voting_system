9f60190abf4115ef79fff76df838f18f2988d255
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-10-27 04:16:53 +0300

    implement test: UserMenuControllerTest.java

---
+++ src/test/java/ru/kopyshev/rvs/web/menu/UserMenuControllerTest.java
@@ -0,0 +1,50 @@
+package ru.kopyshev.rvs.web.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.kopyshev.rvs.service.MenuService;
+import ru.kopyshev.rvs.util.MenuUtil;
+import ru.kopyshev.rvs.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.kopyshev.rvs.MenuTestData.MENU_TO_MATCHER;
+import static ru.kopyshev.rvs.RestaurantTestData.RESTAURANT_ID_1;
+import static ru.kopyshev.rvs.TestData.DATE_1;
+
+class UserMenuControllerTest extends AbstractControllerTest {
+    private static final String restUrl = UserMenuController.REST_URL + "/";
+
+    @Autowired
+    private MenuService service;
+
+    @Test
+    void getAllWithRestaurant() throws Exception {
+        var menuItems = service.getAll(RESTAURANT_ID_1, DATE_1, DATE_1);
+        var menuTosExpected = MenuUtil.getMenuTos(menuItems);
+
+        perform(MockMvcRequestBuilders.get(restUrl + RESTAURANT_ID_1 + "/menu")
+                .param("start", DATE_1.toString())
+                .param("end", DATE_1.toString()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MENU_TO_MATCHER.contentJson(menuTosExpected));
+    }
+
+    @Test
+    void getAll() throws Exception {
+        var menuItems = service.getAll(DATE_1, DATE_1);
+        var menuTosExpected = MenuUtil.getMenuTos(menuItems);
+        perform(MockMvcRequestBuilders.get(restUrl + "menu")
+                .param("start", DATE_1.toString())
+                .param("end", DATE_1.toString()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MENU_TO_MATCHER.contentJson(menuTosExpected));
+    }
+}
