0710802e150b6344d5c332c988d910f8ea7017ce
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-12-26 23:42:42 +0300

    Updated SpringSecurityConfig.java - BCryptPasswordEncoder

--- src/main/java/ru/kopyshev/rvs/config/SpringSecurityConfig.java
+++ src/main/java/ru/kopyshev/rvs/config/SpringSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import ru.kopyshev.rvs.service.UserService;
@@ -48,7 +48,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
