c60a4d976671927da11b0e8de190b6683cf94a51
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-12-28 02:40:51 +0300

    DishUtil.java - Fix lombok @UtilityClass static bug

--- src/main/java/ru/kopyshev/rvs/util/DishUtil.java
+++ src/main/java/ru/kopyshev/rvs/util/DishUtil.java
@@ -10,21 +10,21 @@ import java.util.stream.Collectors;
 @UtilityClass
 public class DishUtil {
 
-    public Dish getDishFromTo(DishTo dishTo) {
+    public static Dish getDishFromTo(DishTo dishTo) {
         var restaurant = RestaurantUtil.getRestaurantFromTo(dishTo.getRestaurant());
         return new Dish(dishTo.getId(), dishTo.getName(), restaurant);
     }
 
-    public List<Dish> getDishFromTo(List<DishTo> dishTos) {
+    public static List<Dish> getDishFromTo(List<DishTo> dishTos) {
         return dishTos.stream().map(DishUtil::getDishFromTo).collect(Collectors.toList());
     }
 
-    public DishTo getToFromDish(Dish dish) {
+    public static DishTo getToFromDish(Dish dish) {
         var restaurantTo = RestaurantUtil.getToFromRestaurant(dish.getRestaurant());
         return new DishTo(dish.getId(), dish.getName(), restaurantTo);
     }
 
-    public List<DishTo> getToFromDish(List<Dish> dishes) {
+    public static List<DishTo> getToFromDish(List<Dish> dishes) {
         return dishes.stream().map(DishUtil::getToFromDish).collect(Collectors.toList());
     }
 }
